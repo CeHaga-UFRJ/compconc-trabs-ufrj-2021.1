b8754c9546a9d25fe97521476d8858f1c63464b3
Markson Arguello <57077854+MarksonArguello@users.noreply.github.com>
2021-08-04 18:28:09 -0300

    Refactored task function

--- trab1/imageEditorConc.c
+++ trab1/imageEditorConc.c
@@ -3,53 +3,89 @@
 #include <pthread.h>
 #include <math.h>
 #include <string.h>
-#include "../lib/timer.h"
+#include "lib/timer.h"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "../lib/stb_image.h"
+#include "lib/stb_image.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../lib/stb_image_write.h"
+#include "lib/stb_image_write.h"
 
 #define ll long long int
 
-unsigned char **output;
+#define DIMENSOES 3
+#define QUALITY 90
+
 char *inputFolderName;
 char *outputFolderName;
 int nthreads;
-int width;
-int height;
 int numFiles;
 
-void *task(void *arg){
-  ll id = (ll)arg;
-  printf("Começou thread %lld\n",id);
-  for(int t = id+1; t <= numFiles; t+=nthreads){
+char* getInputFileName(char* inputFolderName, char* fileNumber) {
     char *fileName = (char *)malloc(sizeof(char));
     strcpy(fileName, inputFolderName);
-    char *fileNumber = (char *)malloc(sizeof(char) * 16);
-    sprintf(fileNumber, "%.4d.jpg", t);
     strcat(fileName, fileNumber);
-    // printf("Reading %s\n",fileName);
-    unsigned char *img = stbi_load(fileName, &width, &height, NULL, 3);
+    return fileName;
+}
+
+char* getOutputFileName(char* outputFolderName, char *fileNumber) {
+    char *fileName = (char *)malloc(sizeof(char));
+    strcpy(fileName, outputFolderName);
+    strcat(fileName, fileNumber);
+    return fileName;
+}
+
+char* getFileNumber(int file) {
+    char *fileNumber = (char *)malloc(sizeof(char) * 16);
+    sprintf(fileNumber, "%.4d.jpg", file);
+    return fileNumber;
+}
+
+unsigned char* getImage(char *fileName,int* width,int* height) {
+    unsigned char *img = stbi_load(fileName, width, height, NULL, DIMENSOES);
     if(!img){
-      printf("Deu merda\n");
+      printf("ERRO--getImage\n");
       exit(1);
     }
+    return img;
+}
+
+unsigned char* convertImage(unsigned char* img, int width, int height) { 
     unsigned char *output = (unsigned char *)malloc(sizeof(unsigned char) * width * height);
-    for(ll i = 0, k = 0; i < width*height*3; i+=3, k++){
+    for(ll i = 0, k = 0; i < width*height*DIMENSOES; i+=DIMENSOES, k++){
       unsigned char red = img[i];
       unsigned char green = img[i + 1];
       unsigned char blue = img[i + 2];
       output[k] = (unsigned char)(red * 30.0/100.0 + green * 59.0/100.0 + blue * 11.0/100.0);
     }
-    strcpy(fileName, outputFolderName);
-    strcat(fileName, fileNumber);
-    if(stbi_write_jpg(fileName, width, height, 1, output, 90) == 0){
-      printf("Não para de dar merda\n");
-      exit(3);
-    }
+    return output;
+}
+
+void writeImage(char *fileName, int width, int height, unsigned char *output) {   
+  if(stbi_write_jpg(fileName, width, height, 1, output, QUALITY) == 0){
+    printf("ERRO--writeImage\n");
+    exit(3);
   }
+}
+
+void *task(void *arg){
+  ll id = (ll)arg;
+  printf("Começou thread %lld\n",id);
+  
+  for(int file = id+1; file <= numFiles; file+=nthreads){
+    int width, height; 
+
+    char* fileNumber      = getFileNumber(file);    
+    char* fileName        = getInputFileName(inputFolderName, fileNumber);
+  
+    unsigned char *image  = getImage(fileName, &width, &height);
+    unsigned char *outputImage = convertImage(image, width, height);
+    
+    
+    fileName = getOutputFileName(outputFolderName, fileNumber);
+    writeImage(fileName, width, height, outputImage);
+  }
+  
   pthread_exit(NULL);
 }
 
@@ -67,22 +103,17 @@ int main(int argc, char *argv[]){
 
 
 
-  output = (unsigned char **)malloc(sizeof(unsigned char *) * numFiles);
-  if(!output){
-    printf("Não aguento mais tanta merda\n");
-    exit(3);
-  }
 
   for(ll i = 0; i < nthreads; i++){
     if(pthread_create(tid+i, NULL, task, (void *)i)){
-      printf("Continua dando merda\n");
+      printf("ERRO--pthread_create\n");
       exit(2);
     }
   }
 
   for(ll i = 0; i < nthreads; i++){
     if(pthread_join(tid[i],NULL)){
-      printf("Merda mais uma vez\n");
+      printf("ERRO--pthread_join\n");
       exit(2);
     }
   }
@@ -95,7 +126,7 @@ int main(int argc, char *argv[]){
   //   strcat(fileName, fileNumber);
   //   // printf("Writing %s\n",fileName);
   //   if(stbi_write_jpg(fileName, width, height, 1, output[i-1], 90) == 0){
-  //     printf("Não para de dar merda\n");
+  //     printf("ERRO--stbi_write_jpg\n");
   //     exit(3);
   //   }
   // }
