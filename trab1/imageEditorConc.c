160555e2b7c0b80091e48ee1e7639e821afaac74
Markson Arguello <57077854+MarksonArguello@users.noreply.github.com>
2021-08-05 20:45:51 -0300

    Refactored main function

--- trab1/imageEditorConc.c
+++ trab1/imageEditorConc.c
@@ -21,16 +21,9 @@ char *outputFolderName;
 int nthreads;
 int numFiles;
 
-char* getInputFileName(char* inputFolderName, char* fileNumber) {
+char* getFileName(char* inputFolderName, char* fileNumber) {
     char *fileName = (char *)malloc(sizeof(char));
     strcpy(fileName, inputFolderName);
-    strcat(fileName, fileNumber);
-    return fileName;
-}
-
-char* getOutputFileName(char* outputFolderName, char *fileNumber) {
-    char *fileName = (char *)malloc(sizeof(char));
-    strcpy(fileName, outputFolderName);
     strcat(fileName, fileNumber);
     return fileName;
 }
@@ -50,7 +43,7 @@ unsigned char* getImage(char *fileName,int* width,int* height) {
     return img;
 }
 
-unsigned char* convertImage(unsigned char* img, int width, int height) { 
+unsigned char* convertImage(unsigned char* img, int width, int height) {
     unsigned char *output = (unsigned char *)malloc(sizeof(unsigned char) * width * height);
     for(ll i = 0, k = 0; i < width*height*DIMENSOES; i+=DIMENSOES, k++){
       unsigned char red = img[i];
@@ -61,7 +54,7 @@ unsigned char* convertImage(unsigned char* img, int width, int height) {
     return output;
 }
 
-void writeImage(char *fileName, int width, int height, unsigned char *output) {   
+void writeImage(char *fileName, int width, int height, unsigned char *output) {
   if(stbi_write_jpg(fileName, width, height, 1, output, QUALITY) == 0){
     printf("ERRO--writeImage\n");
     exit(3);
@@ -71,72 +64,84 @@ void writeImage(char *fileName, int width, int height, unsigned char *output) {
 void *task(void *arg){
   ll id = (ll)arg;
   printf("Começou thread %lld\n",id);
-  
-  for(int file = id+1; file <= numFiles; file+=nthreads){
-    int width, height; 
 
-    char* fileNumber      = getFileNumber(file);    
-    char* fileName        = getInputFileName(inputFolderName, fileNumber);
-  
-    unsigned char *image  = getImage(fileName, &width, &height);
-    unsigned char *outputImage = convertImage(image, width, height);
-    
-    
-    fileName = getOutputFileName(outputFolderName, fileNumber);
+  for(int file = id+1; file <= numFiles; file+=nthreads){
+    int width, height;
+
+    char* fileNumber = getFileNumber(file);
+    char* fileName   = getFileName(inputFolderName, fileNumber);
+
+    unsigned char *inputImage  = getImage(fileName, &width, &height);
+    unsigned char *outputImage = convertImage(inputImage, width, height);
+
+
+    fileName = getFileName(outputFolderName, fileNumber);
     writeImage(fileName, width, height, outputImage);
   }
-  
+
   pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[]){
-  double start, end, delta;
+void initialization(int argc, char *argv[]) {
 
-  GET_TIME(start);
+  if (argc < 4) {
+    printf("Digite: %s <Nome da pasta de entrada> <numero de fotos> <nome da pasta de saida> <numero de threads>\n", argv[0]);
+    exit(1);
+  }
 
   inputFolderName = argv[1];
   numFiles = atoi(argv[2]);
   outputFolderName = argv[3];
   nthreads = atoi(argv[4]);
+}
 
-  pthread_t tid[nthreads];
+pthread_t* createTid() {
+  pthread_t* tid = (pthread_t *) malloc(sizeof(pthread_t) * nthreads);
+  if (tid == NULL) {
+    printf("ERRO--createTid");
+    exit(1);
+  }
+  return tid;
+}
 
-
-
-
+void createThreads(pthread_t* tid) {
   for(ll i = 0; i < nthreads; i++){
     if(pthread_create(tid+i, NULL, task, (void *)i)){
       printf("ERRO--pthread_create\n");
       exit(2);
     }
   }
+}
 
+void waitThreads(pthread_t* tid) {
   for(ll i = 0; i < nthreads; i++){
     if(pthread_join(tid[i],NULL)){
       printf("ERRO--pthread_join\n");
       exit(2);
     }
   }
+}
 
-  // for(int i = 1; i <= numFiles; i++){
-  //   char *fileName = (char *)malloc(sizeof(char));
-  //   strcpy(fileName, outputFolderName);
-  //   char *fileNumber = (char *)malloc(sizeof(char) * 16);
-  //   sprintf(fileNumber, "%.4d.jpg", i);
-  //   strcat(fileName, fileNumber);
-  //   // printf("Writing %s\n",fileName);
-  //   if(stbi_write_jpg(fileName, width, height, 1, output[i-1], 90) == 0){
-  //     printf("ERRO--stbi_write_jpg\n");
-  //     exit(3);
-  //   }
-  // }
+void convert() {
+  pthread_t* tid = createTid();
+  createThreads(tid);
+  waitThreads(tid);
+}
 
 
-  printf("Foi\n");
+int main(int argc, char *argv[]){
+  double start, end, delta;
+
+  GET_TIME(start);
+
+  initialization(argc, argv);
+  convert();
 
   GET_TIME(end);
+
   delta = end - start;
   printf("Tudo levou %f segundos\n",delta);
 
   return 0;
 }
+
