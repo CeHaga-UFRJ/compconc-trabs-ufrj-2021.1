910c1fad4fc331aeff3e934c1f1dcba8cac622a7
Markson Arguello <57077854+MarksonArguello@users.noreply.github.com>
2021-08-25 19:49:09 -0300

    Adding image library include.

--- trab1/compareOutputs.c
+++ trab1/compareOutputs.c
@@ -3,52 +3,12 @@
 #include <pthread.h>
 #include <math.h>
 #include <string.h>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "../lib/stb_image.h"
-
-#define ll long long int
-
-#define DIMENSOES 3
-#define QUALITY 90
+#include "../lib/imagelib.h"
 
 char *seqFolderName;
 char *concFolderName;
 int numFiles;
 
-// Monta o nome do arquivo
-char* getFileName(char* inputFolderName, char* fileNumber) {
-  char *fileName = (char *)malloc(sizeof(char));
-  strcpy(fileName, inputFolderName);
-  strcat(fileName, fileNumber);
-  return fileName;
-}
-
-// Converte um número para o formato "0000"
-char* getFileNumber(int file) {
-  char *fileNumber = (char *)malloc(sizeof(char) * 16);
-  sprintf(fileNumber, "%.4d.jpg", file);
-  return fileNumber;
-}
-
-// Abre uma imagem e define os parâmetros width e height
-unsigned char* getImage(char *fileName,int* width,int* height) {
-  unsigned char *img = stbi_load(fileName, width, height, NULL, DIMENSOES);
-  if(!img){
-    printf("ERRO--getImage\n");
-    exit(1);
-  }
-  return img;
-}
-
-// Retorna se duas imagens sao diferentes
-int compareImages(unsigned char* imgSeq, unsigned char* imgConc, int width, int height) {
-  for(ll i = 0; i < width*height*DIMENSOES; i++){
-    if (imgSeq[i] != imgConc[i])
-        return 1;
-  }
-  return 0;
-}
 
 // Compara as imagens geradas pelo codigo sequencial e pelo codigo concorrente
 void compare() {
--- trab1/imageEditorSeq.c
+++ trab1/imageEditorSeq.c
@@ -4,66 +4,11 @@
 #include <math.h>
 #include <string.h>
 #include "../lib/timer.h"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "../lib/stb_image.h"
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../lib/stb_image_write.h"
-
-#define ll long long int
-
-#define DIMENSOES 3 // Numero de cores na imagem
-#define QUALITY 90 // Qualidade a ser salva
+#include "../lib/imagelib.h"
 
 char *inputFolderName;
 char *outputFolderName;
 int numFiles;
-
-// Monta o nome do arquivo
-char* getFileName(char* inputFolderName, char* fileNumber) {
-  char *fileName = (char *)malloc(sizeof(char));
-  strcpy(fileName, inputFolderName);
-  strcat(fileName, fileNumber);
-  return fileName;
-}
-
-// Converte um número para o formato "0000"
-char* getFileNumber(int file) {
-  char *fileNumber = (char *)malloc(sizeof(char) * 16);
-  sprintf(fileNumber, "%.4d.jpg", file);
-  return fileNumber;
-}
-
-// Abre uma imagem e define os parâmetros width e height
-unsigned char* getImage(char *fileName,int* width,int* height) {
-  unsigned char *img = stbi_load(fileName, width, height, NULL, DIMENSOES);
-  if(!img){
-    printf("ERRO--getImage\n");
-    exit(1);
-  }
-  return img;
-}
-
-// Converte a imagem para escala de cinza
-unsigned char* convertImage(unsigned char* img, int width, int height) {
-  unsigned char *output = (unsigned char *)malloc(sizeof(unsigned char) * width * height);
-  for(ll i = 0, k = 0; i < width*height*DIMENSOES; i+=DIMENSOES, k++){
-    unsigned char red = img[i];
-    unsigned char green = img[i + 1];
-    unsigned char blue = img[i + 2];
-    output[k] = (unsigned char)(red * 30.0/100.0 + green * 59.0/100.0 + blue * 11.0/100.0);
-  }
-  return output;
-}
-
-// Escreve a imagem de saida
-void writeImage(char *fileName, int width, int height, unsigned char *output) {
-  if(stbi_write_jpg(fileName, width, height, 1, output, QUALITY) == 0){
-    printf("ERRO--writeImage\n");
-    exit(3);
-  }
-}
 
 // Converte todas as imagens para escala de cinza
 void convert(){
@@ -84,7 +29,7 @@ void convert(){
     writeImage(fileName, width, height, outputImage);
   }
 
-  pthread_exit(NULL);
+
 }
 
 // Inicializa as variaveis
